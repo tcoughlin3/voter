b4fab9ed7747ff9d6c0d4e8f743000682d50c27f
Tom Coughlin <tcoughlin3@gmail.com>
2016-09-02 13:02:36 -0500

    Adds core app logic
    * `setEntries`, `next`, `vote` functions

---
+++ src/core.js
@@ -0,0 +1,33 @@
+import {List, Map} from 'immutable';
+
+export function setEntries(state, entries) {
+  return state.set('entries', List(entries));
+}
+
+function getWinners(vote) {
+  if (!vote) return [];
+  const [a, b] = vote.get('pair');
+  const aVote = vote.getIn(['tally', a], 0);
+  const bVote = vote.getIn(['tally', b], 0);
+  if (aVote > bVote) return [a];
+  if (bVote > aVote) return [b];
+  return [a, b];
+}
+
+export function next(state) {
+  const winners = getWinners(state.get('vote'));
+  const entries = state.get('entries').concat(winners);
+
+  if (entries.size <= 1) return state.remove('vote').remove('entries').set('winner', entries.first());
+
+  return state.merge({
+    vote: Map({
+      'pair': entries.take(2)
+    }),
+    entries: entries.skip(2)
+  });
+}
+
+export function vote(state, entry) {
+  return state.updateIn(['vote', 'tally', entry], 0, (tally) => tally + 1);
+}
